73e223a93d3313d221002e4930e4695b1b81f6d6
Noah Zajicek <noahzajicek@gmail.com>
2024-02-25 13:00:39 -0500

    Load PUBLIC_DIR from .env file

--- backend/pkg/api/index.go
+++ backend/pkg/api/index.go
@@ -7,7 +7,7 @@ import (
 )
 
 func HandleIndexPage(w http.ResponseWriter, r *http.Request) {
-	workDir, _ := os.Getwd()
-	publicDir := filepath.Join(workDir, "../../../frontend/public/index.html")
-	http.ServeFile(w, r, publicDir)
+	publicDir := os.Getenv("PUBLIC_DIR")
+	indexFilePath := filepath.Join(publicDir, "index.html")
+	http.ServeFile(w, r, indexFilePath)
 }
