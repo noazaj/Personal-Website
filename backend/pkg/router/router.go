767e0ebd814b2bcad5b4342acc4775f2693bb2dc
Noah Zajicek <noahzajicek@gmail.com>
2024-02-27 00:15:22 -0500

    Strip v1 for static file server

--- backend/pkg/router/router.go
+++ backend/pkg/router/router.go
@@ -21,16 +21,20 @@ func NewRouter() *chi.Mux {
 	if publicDir == "" {
 		log.Fatal("PUBLIC_DIR environment variable is not set")
 	}
-	router.Handle("/v1/*", http.FileServer(http.Dir(publicDir)))
+	router.Handle("/*", http.FileServer(http.Dir(publicDir)))
 
 	// Create the subrouter and mount it to the main router
 	v1 := chi.NewRouter()
-	router.Mount("/v1", v1)
 
 	// Define the routes and associate them
 	// with the handlers from the api package
 	v1.Get("/readiness", api.HandlerReadiness)
 	v1.Get("/", api.HandleIndexPage)
+
+	router.Mount("/v1", v1)
+
+	fileServer := http.StripPrefix("/v1", http.FileServer(http.Dir(publicDir)))
+	router.Handle("/v1/*", fileServer)
 
 	return router
 }
