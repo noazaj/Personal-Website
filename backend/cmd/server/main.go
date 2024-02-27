25a0088d3344cd3d830e14955234cd27a225f0af
Noah Zajicek <noahzajicek@gmail.com>
2024-02-26 23:48:18 -0500

    Remove load .env to let Heroku handle variables

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -6,21 +6,14 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/noazaj/Personal-Website/backend/pkg/router"
 )
 
 func main() {
-	// Load env variables
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Fatalf("Couldn't load .env variables. Error: %s", err)
-	}
-
 	// Get the port to be used for the server
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("PORT number must be set in .env file")
+		log.Fatal("PORT number not set")
 	}
 
 	// Create a server
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/noazaj/Personal-Website
 
 go 1.22.0
 
-require (
-	github.com/go-chi/chi/v5 v5.0.12
-	github.com/joho/godotenv v1.5.1
-)
+require github.com/go-chi/chi/v5 v5.0.12
