02e97b2cacd5d6a52cb53cc1457be639cc3b3fcd
Noah Zajicek <noahzajicek@gmail.com>
2024-03-16 01:51:34 -0400

    Add middleware for pathing on website

--- backend/pkg/middleware/middlewarePaths.go
+++ backend/pkg/middleware/middlewarePaths.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -10,6 +11,7 @@ import (
 func MiddlewarePaths(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
+		fmt.Println(path)
 
 		if strings.HasPrefix(path, "/images") || strings.HasPrefix(path, "/css") {
 			util.RespondWithError(w, http.StatusForbidden, "Forbidden Directory")
