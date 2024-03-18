34c580e357111f714e2b45ffb1c7a4aa4d042eca
Noah Zajicek <noahzajicek@gmail.com>
2024-03-17 20:57:01 -0400

    Update permissions to view direcotries on website

--- backend/pkg/middleware/middlewarePaths.go
+++ backend/pkg/middleware/middlewarePaths.go
@@ -11,10 +11,21 @@ func MiddlewarePaths(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
-		if strings.HasPrefix(path, "/images") {
-			util.RespondWithError(w, http.StatusForbidden, "Forbidden Directory")
+		if strings.HasPrefix(path, "/images") || strings.HasPrefix(path, "/css") {
+			if isDirectFilePath(path) {
+				next.ServeHTTP(w, r)
+			} else {
+				util.RespondWithError(w, http.StatusForbidden, "Forbidden Directory")
+			}
 		} else {
 			next.ServeHTTP(w, r)
 		}
 	})
+}
+
+func isDirectFilePath(path string) bool {
+	if strings.HasSuffix(path, ".png") || strings.HasSuffix(path, ".css") {
+		return true
+	}
+	return false
 }
